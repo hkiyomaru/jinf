c9725ed96391b48daba00affb473f7d251ea8779
Hirokazu Kiyomaru <h.kiyomaru@gmail.com>
2022-02-04 00:44:56 +0900

    export version

--- jinf/__init__.py
+++ jinf/__init__.py
@@ -1,5 +1,16 @@
-from jinf.inflection_form import InflectionForm
-from jinf.inflection_type import InflectionType
+from importlib.metadata import version
+
+from jinf.inflection_form import INFECTION_FORMS, InflectionForm
+from jinf.inflection_type import INFLECTION_TYPES, InflectionType
 from jinf.jinf import Jinf
 
-__all__ = ["Jinf", "InflectionForm", "InflectionType"]
+__version__ = version("jinf")
+
+__all__ = [
+    "__version__",
+    "Jinf",
+    "INFECTION_FORMS",
+    "InflectionForm",
+    "INFLECTION_TYPES",
+    "InflectionType",
+]
